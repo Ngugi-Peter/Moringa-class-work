b2aad3ae721456e85897b60b401b923c1cd66fc9
Peter ngugi <peter.ngugi.r@gmail.com>
2026-06-22 10:55:38 +0300

    learned and operators

---
+++ JS/intro/andOrNot.js
@@ -0,0 +1,17 @@
+let coins=10
+let lives=2
+let nextLevel=coins>9 && lives>1
+console.log(`coins=${coins}>9 && lives=${lives}>1 is: ${nextLevel} 
+    it is of type ${typeof nextLevel}`);
+
+    let nextLevel2=coins>15 && lives>3
+console.log(`coins=${coins}>15 && lives=${lives}>3 is: ${nextLevel2} 
+    it is of type ${typeof nextLevel2}`);
+
+    let nextLevel3=coins>15 && lives>1
+console.log(`coins=${coins}>15 && lives=${lives}>1 is: ${nextLevel3} 
+    it is of type ${typeof nextLevel3}`);
+    
+    let nextlevel4=coins>9 && lives>3
+console.log(`coins=${coins}>9 && lives=${lives}>3 is: ${nextlevel4} 
+    it is of type ${typeof nextlevel4}`);
