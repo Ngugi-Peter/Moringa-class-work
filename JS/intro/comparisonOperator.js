d97cc92f579d396c55a1a985334051b98abee52a
Peter ngugi <peter.ngugi.r@gmail.com>
2026-06-22 10:08:40 +0300

    completed comparison operator

--- JS/intro/comparisonOperator.js
+++ JS/intro/comparisonOperator.js
@@ -17,3 +17,37 @@ console.log(`val1=${val1}!==val2=${val2} is strict inequality answer: ${strictin
 console.log(`val1=${val1}!=val2=${val2} is non-strict inequality answer: ${nonStrictInequality} 
     it is of type ${typeof nonStrictInequality}`);
 
+let val3=30
+let val4=40
+
+let strng1="a"
+let strng2="b"
+
+
+let greaterThan=val3>val4
+console.log(`val3=${val3}>val4=${val4} is greater than answer: ${greaterThan} 
+    it is of type ${typeof greaterThan}`);
+    
+
+let lessThan=val3<val4
+console.log(`val3=${val3}<val4=${val4} is less than answer: ${lessThan} 
+    it is of type ${typeof lessThan}`);
+let greaterThanEqual=val3>=val4
+console.log(`val3=${val3}>=val4=${val4} is greater than equal answer: ${greaterThanEqual} 
+    it is of type ${typeof greaterThanEqual}`);
+let lessThanEqual=val3<=val4
+console.log(`val3=${val3}<=val4=${val4} is less than equal answer: ${lessThanEqual} 
+    it is of type ${typeof lessThanEqual}`);
+
+let strngGreaterThan=strng1>strng2
+console.log(`strng1=${strng1}>strng2=${strng2} is greater than answer: ${strngGreaterThan} 
+    it is of type ${typeof strngGreaterThan}`);
+let strngLessThan=strng1<strng2
+console.log(`strng1=${strng1}<strng2=${strng2} is less than answer: ${strngLessThan} 
+    it is of type ${typeof strngLessThan}`);
+let strngGreaterThanEqual=strng1>=strng2
+console.log(`strng1=${strng1}>=strng2=${strng2} is greater than equal answer: ${strngGreaterThanEqual} 
+    it is of type ${typeof strngGreaterThanEqual}`);
+let strngLessThanEqual=strng1<=strng2
+console.log(`strng1=${strng1}<=strng2=${strng2} is less than equal answer: ${strngLessThanEqual} 
+    it is of type ${typeof strngLessThanEqual}`);
