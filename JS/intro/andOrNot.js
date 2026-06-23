21ee9267f910f9e57ffe307a5bf4e9bf92e1c5ac
Peter ngugi <peter.ngugi.r@gmail.com>
2026-06-23 10:22:26 +0300

    and ,or operator and negation

--- JS/intro/andOrNot.js
+++ JS/intro/andOrNot.js
@@ -15,3 +15,21 @@ console.log(`coins=${coins}>15 && lives=${lives}>1 is: ${nextLevel3}
     let nextlevel4=coins>9 && lives>3
 console.log(`coins=${coins}>9 && lives=${lives}>3 is: ${nextlevel4} 
     it is of type ${typeof nextlevel4}`);
+
+    let nextLevel5=coins>9 || lives>1
+console.log(`coins=${coins}>9 || lives=${lives}>1 is: ${nextLevel5} `);
+
+let nextLevel6=coins>15 || lives>3
+console.log(`coins=${coins}>15 || lives=  ${lives}>3 is: ${nextLevel6} `);
+
+let nextLevel7=coins>15 || lives>1
+console.log(`coins=${coins}>15 || lives=  ${lives}>1 is: ${nextLevel7} `);
+
+let nextlevel8=coins>9 || lives>3
+console.log(`coins=${coins}>9 || lives=  ${lives}>3 is: ${nextlevel8} `);
+
+let nextlevel6=!(coins>9 || lives>1)
+console.log(nextlevel6);
+
+let nextlevel7=!(coins>15 || lives>3)
+console.log(`!(coins=${coins}>15 || lives=  ${lives}>3) is: ${nextlevel7} `);
