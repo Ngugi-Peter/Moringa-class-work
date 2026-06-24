ce694920d7d15644c88165986ade468048ee2deb
Peter ngugi <peter.ngugi.r@gmail.com>
2026-06-24 10:58:20 +0300

    deep dive into If condition

---
+++ JS/intro/ifOnly.js
@@ -0,0 +1,26 @@
+let age= prompt("Enter your age");
+alert(`Enter age ${age}`);
+
+if (age>10) {
+    alert("You are baby");
+}
+
+if (age>15) {
+    alert("You are teenager");
+}
+
+if(age>18) {
+    alert("You are adult");
+}
+
+if(age>30) {
+    alert("you can marry");
+}
+
+if(age>40) {
+    alert("you can retire");
+}
+
+if(age>50) {
+    alert("you are old");
+}
