a346f83c36d7f7007e61ffa1c5207717d619b3be
Peter ngugi <peter.ngugi.r@gmail.com>
2026-06-24 10:05:43 +0300

    if condition

---
+++ JS/intro/condition.js
@@ -0,0 +1,21 @@
+let batteryOn = true;
+if (batteryOn) {
+  console.log("Scenario 1 it run.");}
+
+batteryOn = false;
+if (batteryOn) {
+  console.log("Scenario 2 it run.");
+}
+if (true) {
+  console.log("Scenario 3 it run.");
+}
+if (false) {
+  console.log("Scenario 4 it run.");
+}
+
+if (10>20) {
+  console.log("Scenario 5 it run.");
+}
+if (10<20) {
+  console.log("Scenario 6 it run.");
+}
