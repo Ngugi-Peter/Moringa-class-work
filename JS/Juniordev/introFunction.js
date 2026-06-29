04b3fa4953c22eb542814dbb3d809ff7c985b9e1
Peter ngugi <peter.ngugi.r@gmail.com>
2026-06-29 14:11:08 +0300

    introduction to function

---
+++ JS/Juniordev/introFunction.js
@@ -0,0 +1,42 @@
+/*
+functions allow us to write ussuable bits of code that can be called when needed. 
+Functions can take in parameters and return values.
+big rule function only work when called
+calling a function is telling it to run.
+*/
+function timeAlert() {
+    const date = new Date();
+    alert(`time stamp ${date.toISOString()}`);
+}
+
+/* to call a function we use function name and then brackets
+timeAlert()
+*/
+
+//timeAlert();
+
+function areaOfTriangle(base, height) {
+    console.log(`base length is ${base} and type is ${typeof base}`);
+    console.log(`height is ${height} and type is ${typeof height}`);
+    const area = 0.5 * base * height;
+    console.log(`for triangle with base ${base} and height ${height}, the area is ${area}`);
+}
+
+// scenario 1 call the function without parameters or arguments
+areaOfTriangle();
+
+// scenario 2 call the function with justone parameter and argument
+areaOfTriangle(10);
+
+// scenario 3 call the function with both parameters and arguments
+areaOfTriangle(10, 20);
+
+//scenario 4 create two variables and use variable name as arguments
+const base = 10;
+const height = 20;
+areaOfTriangle(base, height);
+
+// scenario 5 call the function with both parameters and arguments but with different data types
+const base1 = "10";
+const height1 = true;
+areaOfTriangle(base1, height1);
--- JS/intro/salaryCalculator/index.js
+++ JS/intro/salaryCalculator/index.js
@@ -47,3 +47,66 @@ else{
     alert(`Your PAYE is ${paye}`);
     console.log(`Your PAYE is ${paye}`);
 }
+let Nssf = 0;
+let totalNssf = Nssf*2;
+
+if (grossMonthySalary > 9000) {
+    Nssf = grossMonthySalary * 0.06;
+    totalNssf = Nssf*2;
+    alert(`Your NSSF is ${totalNssf}. You are in Tier 1`);
+}
+else if (grossMonthySalary >= 9000 && grossMonthySalary <= 108000) {
+    Nssf = grossMonthySalary * 0.06;
+    totalNssf = Nssf*2;
+    alert(`Your NSSF is ${totalNssf}. You are in Tier 2`);
+}
+else {
+    Nssf = 6480;
+    totalNssf = Nssf*2;
+    alert(`Your NSSF is ${totalNssf}. You are in Tier 3`);
+}
+// taxable income is calculated after deducting NSSF from gross salary 
+let taxableIncome = grossMonthySalary - totalNssf;
+// to get paye we need to run our taxable income through the paye calculator
+// PAYE calculator
+let shif = grossMonthySalary * 0.0275;
+let housingLevy =grossmonthysalary * 0.15;
+
+const bracket1 = 240000
+const bracket2 =32333
+const bracket3 = 500000
+const bracket4 = 800000
+
+const band1 = bracket1 * 0.1;
+const band2 = (bracket2-bracket1)*0.25;
+const band3 = (bracket3-bracket2)*0.3;
+const band4 = (bracket4-bracket3)*0.325;
+
+const personalRelief=2400;
+
+if(taxableIncome<=24000){
+    paye= taxableIncome*0.1
+    paye_tier= "paye 0-24000kes";
+}else if(taxableIncome <=32333){
+    let diff=taxableIncome-24000;
+    let tax=diff *0.25;
+    paye= tax+ band1;
+    paye_tier = 'PAYE 24000-32333 KES';
+}else if(taxableIncome <=500000){
+   let diff=taxableIncome-24000;
+    let tax=diff *0.25;
+    paye= band2 + tax  + band1;
+    paye_tier = 'PAYE 500000-32333 KES' 
+}else if(taxableIncome <=800000){
+let diff=taxableIncome-500000;
+    let tax=diff *0.325;
+    paye= band3 + band2 + tax  + band1;
+    paye_tier = 'PAYE 800000 - 500000 KES'
+}else {
+    let diff=taxableIncome-800000;
+    let tax=diff *0.35;
+    paye= band4 + band3 + band2 + tax  + band1;
+    paye_tier = 'PAYE 800000 to infinity KES'
+}
+
+const final_paye = Math.max(0, paye -  personalRelief)
