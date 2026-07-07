ee82dbc8e02f91c3f65c3f46084aa5809603ec69
Peter ngugi <peter.ngugi.r@gmail.com>
2026-07-08 00:52:22 +0300

    introduction to for loops

---
+++ JS/Juniordev/forloops.js
@@ -0,0 +1,23 @@
+/* for loop 
+they are mostly used for number of iterations
+combination of using while for numbers and loops
+1.starting 2.condition 3.after each iteration
+combines it into one line of code 
+for(starting point; condition; after each iteration) {
+    //code to be executed
+}
+*/
+
+let k = 0;// starting point
+
+while(k<20) {
+    console.log("k is",k);
+    k=k+1;//incrementing
+}
+
+for(let k = 0; k < 20; k++) {
+    console.log("k is",k);
+}
+
+
+
