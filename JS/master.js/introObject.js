ccd2cc8fd54fc5f559482bff1dbb7c6651ca81ac
Peter ngugi <peter.ngugi.r@gmail.com>
2026-07-09 10:34:43 +0300

    introduction to Objects

---
+++ JS/master.js/introObject.js
@@ -0,0 +1,23 @@
+const car = {
+    name : "porsche",
+    model : "911",
+    year : 2020,
+    color : "red",
+    cylinders : 6,
+    "top speed": 200,
+
+    manufacturer : {
+        name : "porsche AG",
+        country : "Germany",
+        "plate number" : 2354632,
+        alert : function() {
+            console.log(car);
+        }
+      292: "this is the regestration number of the car",
+      "fun fact" : `Another interesting fact: the 911 was almost called the 901.
+       When Porsche introduced the car in 1963, it was named the 901. However, 
+       Peugeot held trademark rights in France
+       for three-digit car names with a zero in the middle (like 404 or 504)`
+    }   
+
+}
