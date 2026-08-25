7a32a15cf4d84ed897fbcc30f2883c9b5f269e77
Peter ngugi <peter.ngugi.r@gmail.com>
2026-08-25 10:19:02 +0300

    intro to Python

---
+++ Python/data_types.py
@@ -0,0 +1,18 @@
+#Example varaiables
+x=10 #integer
+y=4,56  # Float
+e="Errror"  # String
+a= True    # Boolean
+b =[2, 3, 5] # list
+g=(5, 6, 7) #Tuple
+d={3, 4, 5} #set
+h={"key", "value"} #Dictionary
+
+
+print ("X is ",x,"Its type ",type(x))
+print ("Y is ",y,"Its type ",type(y))
+print ("E is ",e,"Its type ",type(e))
+print ("A is ",a,"Its type ",type(a))
+print ("B is ",b,"Its type ",type(b))
+print ("G is ",g,"Its type ",type(g))
+print ("D is ",d,"Its type ",type(d))
---
+++ Python/start.py
@@ -0,0 +1,9 @@
+# is used to make comments in python
+x= 24
+z=45
+# we dont have to declare variables in python
+# we also dont use console.log we use print 
+y=x+z
+print (y)
+print (z)
+print (x)
